03556bc4174193fcc302dde55e1313ef8cd3d3c0
Prakash Paswan <82628149+prakashptech@users.noreply.github.com>
2022-09-04 00:05:19 +0530

    fetchmethodadded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
-
+//this is link article is forusing fetch method
+//https://stackoverflow.com/questions/50075818/api-call-using-fetch-with-method-get
 const App = () => {
   return <div>h</div>;
 };
